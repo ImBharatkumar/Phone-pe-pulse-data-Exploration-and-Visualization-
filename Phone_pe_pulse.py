a779615856a2caed807673e1f15391f482c43c61
Bharatkumar kori <118038590+ImBharatkumar@users.noreply.github.com>
2023-03-07 02:17:25 +0530

    Update Phone_pe_pulse.py

--- Phone_pe_pulse.py
+++ Phone_pe_pulse.py
@@ -177,3 +177,17 @@ df = pd.read_csv(
 )
 Data = st.dataframe(filter_dataframe(df))
 print(Data)
+
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
